35ed873dc57665d446d84a8854f4c4752a16c2b1
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-11-02 20:51:12 +0200

    Added file

--- Pamoka_1/index.php
+++ Pamoka_1/index.php
@@ -1,26 +1,27 @@
 <?php
- if (isset($_POST['send_plus'])) {
+ if ((isset($_POST['send_plus'])) && (isset($_POST['myValue'])) && (isset($_POST['NextMyValue']))) {
      echo "Gautas skaicius sudejus ";
      echo $_POST['myValue'] + $_POST['NextMyValue'];
      }
- if (isset($_POST['send_minus'])) {
+ if ((isset($_POST['send_minus'])) && (isset($_POST['myValue'])) && (isset($_POST['NextMyValue']))) {
      echo "Gautas skaicius atemus ";
      echo $_POST['myValue'] - $_POST['NextMyValue'];
      }
- if (isset($_POST['send_daugyba'])) {
+ if ((isset($_POST['send_daugyba'])) && (isset($_POST['myValue'])) && (isset($_POST['NextMyValue']))) {
      echo "Gautas skaicius sudauginus ";
      echo ($_POST['myValue'] * $_POST['NextMyValue']);
      }
- if (isset($_POST['send_dalyba'])) {
+ if ((isset($_POST['send_dalyba'])) && (isset($_POST['myValue'])) && (isset($_POST['NextMyValue']))) {
      echo "Gautas skaicius padalinus ";
      echo ($_POST['myValue'] / $_POST['NextMyValue']);
      }
 ?>
+<div>Įveskite du skaičius</div>
 <form id="first_form" name="first_form" method="POST" enctype="multipart/form-data" target="_self">
 
-    <input name="myValue" type="number"><br>
+    Pirmas skaičius: <input name="myValue" type="number"><br>
 
-    <input name="NextMyValue" type="number"><br><br>
+    Antras skaičius:<input name="NextMyValue" type="number"><br><br>
 
     <button name="send_plus" type="submit">Sudeti</button>&nbsp;
     <button name="send_minus" type="submit">Atimti</button>&nbsp;

2a1ea87c927dd6e48f19cf81b0bf26c261820948
Sigitas Sukaitis <sigitas@danis.lt>
2021-11-02 21:48:59 +0200

    bug fix

--- Pamoka_1/index.php
+++ Pamoka_1/index.php
@@ -1,9 +1,6 @@
 <?php
 
 $random = rand(0,10);
-echo "<br>Veiksmas atliktas su random skaiciumi: " . $random;
-
-echo '<input id="rand" name="rand" value="'.$random.'">';
 
 if ((isset($_POST['send_plus'])) && (isset($_POST['myValue'])) && (isset($_POST['NextMyValue']))) {
 echo "Gautas skaicius sudejus ";
@@ -21,14 +18,14 @@ if ((isset($_POST['send_dalyba'])) && (isset($_POST['myValue'])) && (isset($_POS
 echo "Gautas skaicius padalinus ";
 echo ($_POST['myValue'] / $_POST['NextMyValue'] / $_POST['rand']);
 }
-
+echo "<br>Veiksmas bus atliktas su random skaiciumi: " . $random."<br>";
 
 ?>
 <form id="first_form" name="first_form" method="POST" enctype="multipart/form-data" target="_self">
+<?php echo '<input hidden="hidden" id="rand" name="rand" value="'.$random.'">'; ?>
+<input name="myValue" type="number" required><br>
 
-<input name="myValue" type="number"><br>
-
-<input name="NextMyValue" type="number"><br><br>
+<input name="NextMyValue" type="number" required><br><br>
 
 <button name="send_plus" type="submit">Sudeti</button>&nbsp;
 <button name="send_minus" type="submit">Atimti</button>&nbsp;

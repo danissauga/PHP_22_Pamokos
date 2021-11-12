bee134726e1f99c2b2c496e4a73db68d16b363d9
Sigitas Sukaitis <sigitas@danis.lt>
2021-11-12 21:33:44 +0200

    Update pamoka_9

--- Pamoka_9/loops.php
+++ Pamoka_9/loops.php
@@ -1,13 +1,13 @@
 <style type="text/css">
 p {
-        max-width: 500px;
+        max-width: auto;
 }
 span {
     color: red;
 }
 
 .break_data {
-            word-break: break-all;
+            word-break: normal;
 }
 
 </style>
@@ -19,12 +19,12 @@ $more_then_150 = 0;
 echo '<p class="break_data">';
 for ($st = $start; $st < $stop; $st++ ) {
       $who = rand($start, $stop);
-      if ($who > 275) {echo "<span>$who </span>"; }
-        else { echo $who.' ';}
+      if ($who > 275) {echo "<span> $who</span>"; }
+        else { echo ' '.$who;}
       if ($who > 150) { $more_then_150++;}
 }
 echo '<br> Skaiciu didesniu uz 150 yra: '.$more_then_150;
 echo '</p>';
 
-
 ?>
+

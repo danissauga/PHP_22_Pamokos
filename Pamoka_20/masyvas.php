9fd8cb7a9ed00d7c61e628412d5ef6c7f9d9a658
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-12-02 22:52:07 +0200

    Update masyvas.php
    Updeites

--- Pamoka_20/masyvas.php
+++ Pamoka_20/masyvas.php
@@ -3,21 +3,38 @@ echo '<pre>';
 $masyvas_10 = [];
 $masyvo_vidus = [];
 settype($raides, "string");
-$raides = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$raides = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 for ($i = 0; $i < 10; $i++) {
     for ($x = 0; $x <= rand(2,20); $x++) {
-        $kuri_raide = rand(0,10);
+        $kuri_raide = rand(0,strlen($raides)-1);
 
         $masyvas_10[$i][$x] = $raides[$kuri_raide];
     }
+    asort($masyvas_10[$i]);
 }
-echo 'Pradiniai duomenys: <br>';
+
+$temp_array_su_K = [];
+$temp_array_be_K = [];
+
+echo 'Pradinis surusiuotas masyvas: <br>';
 print_r($masyvas_10);
-//rusiuojame
+
 for ($i = 0; $i < 10; $i++) {
-   asort($masyvas_10[$i]);
+
+   if (in_array('K',$masyvas_10[$i])) {
+      $temp_array_su_K[] = $masyvas_10[$i];
+      } else {
+          $temp_array_be_K[] = $masyvas_10[$i];
+      }
 }
-echo 'Surusiuota: <br>';
+
+$masyvas_10=[];
+asort($temp_array_su_K);
+asort($temp_array_be_K);
+
+$masyvas_10 = array_merge($temp_array_su_K,$temp_array_be_K);
+echo 'Su K pirmieji, be K visi like masyvas: <br>';
 print_r($masyvas_10);
+
 ?>

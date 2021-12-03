dc131d2a2c651b16079dd924fb35dbe79164f3a1
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-12-03 22:40:38 +0200

    Uzduotis6 add
    New task 6 add

---
+++ Pamoka_23/uzduotis_6.php
@@ -0,0 +1,13 @@
+<?php
+$symbols = [];
+$data_array = [];
+echo '<pre>';
+for ($i=0; $i< 10; $i++) {
+    for ($x=0; $x< 10; $x++) {
+        $data_array[$i][$x]['value'] = "test";
+        $data_array[$i][$x]['color'] = "red";
+    }
+}
+print_r($data_array);
+
+?>

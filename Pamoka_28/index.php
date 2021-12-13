7b878e2fb9a8257065139dceb4d661a1d71f5996
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-12-13 21:01:49 +0200

    Add updates

--- Pamoka_28/index.php
+++ Pamoka_28/index.php
@@ -7,7 +7,7 @@ for ($i=0; $i<100; $i++ ){
     }
 }
 
-//echo '<pre>';
+echo '<pre>';
 //print_r($data_mass);
 
 $json_encoded_data=json_encode($data_mass);
@@ -17,16 +17,28 @@ $failas = fopen($target,'w+');
 if(fwrite($failas, $json_encoded_data)) {
     echo 'Duomenys į failą įrašyti sėkmingai<br>';
 };
-fclose($failas);
 
-$data = fopen($target, "r");
-//Output lines until EOF is reached
-while(! feof($data)) {
-    $line = fgets($data);
-    echo $line. "<br>";
+$json_encoded_data = file_get_contents($target, $json_encoded_data);
+
+$data_mass = json_decode($json_encoded_data);
+
+foreach($data_mass as $id => $value){
+    foreach ($value as $idd => $value2) {
+        if ($value2 > 6500) {
+            unset($data_mass[$id][$idd]);
+        }
+    }
+
 }
+print_r($data_mass);
 
-fclose($data);
+$json_encoded_data=json_encode($data_mass);
 
+$failas = fopen($target,'w+');
+if(fwrite($failas, $json_encoded_data)) {
+    echo 'Duomenys į failą įrašyti sėkmingai<br>';
+};
+
+fclose($failas);
 
 ?>

be6ea3679c225cbc9bf97305ec525d2acc00d986
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-12-16 23:15:23 +0200

    Add start sours of Bankas
    Add start sours of Bankas

---
+++ Bankas/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+-->
+     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+     <link rel="stylesheet" href="css/styles.css">
+
+</head>
---
+++ Bankas/index.php
@@ -0,0 +1,39 @@
+<?php
+include("includes/header.php");
+session_start();
+include("includes/login.php");
+    if (isset($_POST['logoff'])) {logoff(); }
+    if (isset($_POST['go'])) {login($_POST,$users);}
+
+if (!is_logged_in($users)) :
+?>
+<div class="container h-100">
+         <div class="row h-100 justify-content-center align-items-center">
+
+
+                            <form id="login" class="col-5" name="login" method="POST" enctype="multipart/form-data">
+                                 <h1 class="h3 mb-3 fw-normal">Login form</h1>
+                                <div class="form-floating mb-3">
+                                    <input type="email" name="uname" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="password" name="upass" class="form-control" id="floatingPassword" placeholder="Password">
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <div class="checkbox mb-3">
+                                </div>
+                                <button name="go" class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                            </form>
+
+
+         </div>
+</div>
+
+
+<?php else :
+include("pages/bankas.php"); //Aciu inga :)
+endif;
+include("includes/footer.php");
+?>
+

270e809ecbb531a12614e7fae0b11439a3ad9282
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2021-12-29 23:20:30 +0200

    Update index.php

--- Pamoka_36/index.php
+++ Pamoka_36/index.php
@@ -1,14 +1,41 @@
 <?php
 
 echo '<pre>';
-
+// 1,2,3 uzduoties
 $mass = new stdClass;
 $element=0;
 
+//tik 1 mos uzduoties
 for ($i=0; $i<30; $i++ ) {
     $mass->$i = rand(5,25);
 }
+
+
+foreach ($mass as $key => $value) {
+
+  if ($value > 10) {
+      echo 'Pirmasis maziausias indeksas kurio value > 10 yra: '. $key. '<br />';
+      break;
+  }
+
+}
+
+
+// 2 ir 3 cio uzduoties
 $count = count((array)$mass);
+//tik 3 cios uzduoties
+for ($i=0; $i<$count; $i++) {
+    if ((fmod($i,2)) and ($mass->$i > 15)) {
+        $mass->$i = 0;
+    }
+}
+print_r($mass);
+
+//4 ta uzduotis
+
+
+
+// 2 uduotis
 for ($i=$count-1; $i<$count+10; $i++) {
     $mass->$i = rand(5,25);
 }

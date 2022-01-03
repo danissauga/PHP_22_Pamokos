8d9ddd211bbf04acad3f7013be9adecd2580fcc6
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-03 23:13:46 +0200

    Update Composer Simplon/Mysqli pacade using
    Update Composer Simplon/Mysqli pacade using

--- Pamoka_37/composer.php
+++ Pamoka_37/composer.php
@@ -1,38 +1,90 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+spl_autoload_register(function() {
+    require __DIR__ . '/Crud/NameStore.php';
+    require __DIR__ . '/Crud/NameModel.php';
+    require __DIR__ . '/Crud/ExampleClass.php';
+});
+
+include (__DIR__ . '/vendor/autoload.php');
+
+use Simplon\Mysql\Mysql;
+use Simplon\Mysql\Crud\CrudModel;
+use Simplon\Mysql\Crud\CrudModelInterface;
+use Simplon\Mysql\Crud\CrudStore;
+use Simplon\Mysql\MysqlException;
+use Simplon\Mysql\QueryBuilder\CreateQueryBuilder;
+use Simplon\Mysql\QueryBuilder\DeleteQueryBuilder;
+use Simplon\Mysql\QueryBuilder\ReadQueryBuilder;
+use Simplon\Mysql\QueryBuilder\UpdateQueryBuilder;
+
+use Crud\NameStore;
+use Crud\NameModel;
+use Crud\ExampleClass;
 
 $host     = "localhost";//Ip of database, in this case my host machine
 $user     = "admin";	//Username to use
 $pass     = "admin";//Password for that user
 $dbname   = "blogas";//Name of the database
 
-try {
         $connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-}catch(PDOException $e)
-{
-    echo $e->getMessage();
-}
 
-//phpinfo();
+$dbConn = new Mysql($connection);
+$store = new NameStore($dbConn);
+$duom = new ExampleClass();
+
+//Prideti irasa
+
+ $from_input = [
+    'name' => 'Peter',
+    'age'  => 46,
+];
+
+$duom->AddRecord($dbConn, $from_input);
+
+//Duomenu atnaujinimas
+
+    $conds = ['id' =>5,];
+
+    $from_input = [
+            'name' => 'Gabi',
+            'age'  => 50,
+    ];
+
+$duom->UpdateRecord($dbConn,$conds,$from_input);
+
+//duomenu panaikinimas
+
+$duom->DeleteRecord($dbConn,3);
+
+//Visu duomenu panaikinimas
+
+//$duom->DeleteAllRecords($dbConn);
+
+//Custom duomenu panaikinimas
+
+$from_input = [
+    'id'   => 5,
+    'name' => 'John',
+];
+
+//$duom->DeleteCustomRecord($dbConn, $from_input);
+
+
+//Duomenu paemimas
+$model = $store->read( new ReadQueryBuilder() );
+$duom->ViewRecord($model);
+echo '<br />';
 
 
 
 
-/*$pdo = new PDOConnector(
-	'localhost', // server
-	'',      // user
-	'',      // password
-	'blogas'   // database
-);
 
-$pdoConn = $pdo->connect('utf8', []); // charset, options
 
-//
-// you could now interact with PDO for instance setting attributes etc:
-// $pdoConn->setAttribute($attribute, $value);
-//     */
 
-$dbConn = new Simplon\Mysql\Mysql($connection);
+
+
+
+

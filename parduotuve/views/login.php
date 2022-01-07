7a8886cafcf3fd55d61441383d35346a1a61a36b
Danis sauga, Sigita Sukaitis <73879417+danissauga@users.noreply.github.com>
2022-01-07 18:23:09 +0200

    Update login.php

--- parduotuve/views/login.php
+++ parduotuve/views/login.php
@@ -4,7 +4,7 @@ include("views/meniu.php");
 ?>
     <div class="container h-75">
              <div class="row h-75 justify-content-center align-items-center">
-                    <form id="login" class="col-5" action="<?php echo ROOT; ?>" name="login" method="POST" enctype="multipart/form-data">
+                    <form id="login" class="col-5" action="login" name="login" method="POST" enctype="multipart/form-data">
                          <h1 class="h3 mb-3 fw-normal">Vartotojo prisijungimas</h1>
                         <div class="form-floating mb-3">
                             <input type="email" name="uname" class="form-control" id="floatingInput" placeholder="name@example.com">
